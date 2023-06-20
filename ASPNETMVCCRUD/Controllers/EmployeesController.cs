4ba26979ef1d2d26d9c695d9f4e25e3cf46f7c8c
Nabil <100967334+thraxy@users.noreply.github.com>
2023-06-20 13:22:40 -0400

    Initial commit

---
+++ ASPNETMVCCRUD/Controllers/EmployeesController.cs
@@ -0,0 +1,44 @@
+﻿using ASPNETMVCCRUD.Data;
+using ASPNETMVCCRUD.Models;
+using ASPNETMVCCRUD.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+using MySql.EntityFrameworkCore;
+
+namespace ASPNETMVCCRUD.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly MVCDemonDbContext mvcDemonDbContext;
+
+        public EmployeesController(MVCDemonDbContext mvcDemonDbContext)
+        {
+            this.mvcDemonDbContext = mvcDemonDbContext;
+        }
+
+
+        // add a new employee
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(AddEmployeeViewModel addEmployeeRequest)
+        {
+            var employee = new Employee()
+            {
+                Id = Guid.NewGuid(),
+                Name = addEmployeeRequest.Name,
+                Email = addEmployeeRequest.Email,
+                Salary = addEmployeeRequest.Salary,
+                Department = addEmployeeRequest.Department,
+                DateOfBirth = addEmployeeRequest.DateOfBirth
+            };
+
+            await mvcDemonDbContext.Employees.AddAsync(employee);
+            await mvcDemonDbContext.SaveChangesAsync();
+            return RedirectToAction("Add");
+        }
+    }
+        
+}
---
+++ ASPNETMVCCRUD/Data/MVCDemonDbContext.cs
@@ -0,0 +1,20 @@
+﻿using ASPNETMVCCRUD.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+using MySql.EntityFrameworkCore;
+
+namespace ASPNETMVCCRUD.Data
+{
+    /// MVCDEMO inherits from a base class file from DbContext from the EntityFrameworkCore
+    public class MVCDemonDbContext : DbContext
+    {
+        // constructor for MVCDEMO 
+        public MVCDemonDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        // Db of Employee
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
